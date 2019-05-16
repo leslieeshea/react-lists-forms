de89fe5ca19a654b9d3de8f340218366bbbb2dce
Leslie Shea <leslieyshea@gmail.com>
2019-05-16 11:06:48 -0700

    renders correctly on browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import ColorList from '../containers/ColorList';
 
 export default function App() {
   return (
     <>
       <Header />
+      <ColorList />
     </>
   );
 }
