8e3d0482d8980df02a60e62713288e9e20805d29
Leslie Shea <leslieyshea@gmail.com>
2019-05-16 11:18:56 -0700

    handles submitting a new color test passing

--- src/components/colors/CreateColor.test.js
+++ src/components/colors/CreateColor.test.js
@@ -23,4 +23,14 @@ describe('CreateColor component', () => {
       target: { name: 'color', value: '#FF0000' }
     });
   });
+
+  it('handles submitting on new color', () => {
+    const submit = jest.fn();
+    const wrapper = shallow(<CreateColor addColor={submit} />);
+    wrapper.find('form').at(0).simulate('submit', {
+      preventDefault: jest.fn()
+    });
+
+    expect(submit).toHaveBeenCalledWith({ name: wrapper.state('name'), hex: wrapper.state('color') });
+  });
 });
